e2e19f17f1e45ffad59c55617d933575c4a0d50a
ichizokichinosuke <nuuuukemw@gmail.com>
2020-06-19 18:50:31 +0900

    add voice

--- Resources/Voices/server.ts
+++ Resources/Voices/server.ts
@@ -6,5 +6,5 @@ const fs = require('fs')
 const voice = new VoiceText('3xmx4v5t04hum24n')
 voice.speaker(voice.SPEAKER.HIKARI)
     .emotion(voice.EMOTION.HAPPINESS) // NONE, HAPPINESS, ANGER, SADNESS
-    .speak('「Yes we きゃん」　君ならできる！', (e, buf) => {fs.writeFile('./YesWeCan.wav', buf, 'binary', (e) => {console.error(e)})
+    .speak('何してるの?', (e, buf) => {fs.writeFile('./happy2.wav', buf, 'binary', (e) => {console.error(e)})
 })
